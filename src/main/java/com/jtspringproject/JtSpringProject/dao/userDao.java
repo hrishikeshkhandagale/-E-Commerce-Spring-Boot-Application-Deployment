9ea5bdf000dad65a8bf0313d86b7df84538dceaf
hrishikeshkhandagale <hrishikeshkhandagale2@gmail.com>
2025-11-29 16:26:20 +0530

    modified

--- src/main/java/com/jtspringproject/JtSpringProject/dao/userDao.java
+++ src/main/java/com/jtspringproject/JtSpringProject/dao/userDao.java
@@ -2,82 +2,63 @@ package com.jtspringproject.JtSpringProject.dao;
 
 import java.util.List;
 
-import javax.persistence.NoResultException;
-import javax.sound.midi.Soundbank;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jtspringproject.JtSpringProject.models.User;
 
-
 @Repository
+@Transactional
 public class userDao {
-	@Autowired
-    private SessionFactory sessionFactory;
-	
-	public void setSessionFactory(SessionFactory sf) {
-        this.sessionFactory = sf;
-    }
-   @Transactional
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public List<User> getAllUser() {
-        Session session = this.sessionFactory.getCurrentSession();
-		List<User>  userList = session.createQuery("from CUSTOMER").list();
-        return userList;
+        return entityManager.createQuery("from CUSTOMER", User.class).getResultList();
     }
-    
-    @Transactional
-	public User saveUser(User user) {
-		this.sessionFactory.getCurrentSession().saveOrUpdate(user);
-		System.out.println("User added" + user.getId());
+
+    public User saveUser(User user) {
+        entityManager.merge(user);
+        System.out.println("User added: " + user.getId());
         return user;
-	}
-    
-//    public User checkLogin() {
-//    	this.sessionFactory.getCurrentSession().
-//    }
-    @Transactional
-    public User getUser(String username,String password) {
-    	Query query = sessionFactory.getCurrentSession().createQuery("from CUSTOMER where username = :username");
-    	query.setParameter("username",username);
-    	
-    	try {
-			User user = (User) query.getSingleResult();
-			System.out.println(user.getPassword());
-			if(password.equals(user.getPassword())) {
-				return user;
-			}else {		
-				return new User();
-			}
-		}catch(Exception e){
-			System.out.println(e.getMessage());
-			User user = new User();
-			return user;
-		}
-    	
     }
 
-	@Transactional
-	public boolean userExists(String username) {
-		Query query = sessionFactory.getCurrentSession().createQuery("from CUSTOMER where username = :username");
-		query.setParameter("username",username);
-		return !query.getResultList().isEmpty();
-	}
+    public User getUser(String username, String password) {
+        try {
+            Query query = entityManager.createQuery("from CUSTOMER where username = :username");
+            query.setParameter("username", username);
+            User user = (User) query.getSingleResult();
 
-	@Transactional
-	public User getUserByUsername(String username) {
-	        Query<User> query = sessionFactory.getCurrentSession().createQuery("from User where username = :username", User.class);
-	        query.setParameter("username", username);
-	        
-	        try {
-	            return query.getSingleResult();
-	        } catch (Exception e) {
-	            System.out.println(e.getMessage());
-	            return null; 
-	        }
-    	}
+            if (password.equals(user.getPassword())) {
+                return user;
+            } else {
+                return new User();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new User();
+        }
+    }
+
+    public boolean userExists(String username) {
+        Query query = entityManager.createQuery("from CUSTOMER where username = :username");
+        query.setParameter("username", username);
+        return !query.getResultList().isEmpty();
+    }
+
+    public User getUserByUsername(String username) {
+        try {
+            Query query = entityManager.createQuery("from CUSTOMER where username = :username");
+            query.setParameter("username", username);
+            return (User) query.getSingleResult();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
