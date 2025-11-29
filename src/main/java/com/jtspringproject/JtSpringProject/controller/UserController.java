627c9244a2902f6c50be6725fd8f05d1bde40f3a
hrishikeshkhandagale <hrishikeshkhandagale2@gmail.com>
2025-11-29 20:01:16 +0530

    modified

--- src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
@@ -2,10 +2,7 @@ package com.jtspringproject.JtSpringProject.controller;
 
 import com.jtspringproject.JtSpringProject.models.Product;
 import com.jtspringproject.JtSpringProject.models.User;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,84 +17,103 @@ import com.jtspringproject.JtSpringProject.services.productService;
 @Controller
 public class UserController {
 
-	private final userService userService;
-	private final productService productService;
+    private final userService userService;
+    private final productService productService;
 
-	@Autowired
-	public UserController(userService userService, productService productService) {
-		this.userService = userService;
-		this.productService = productService;
-	}
+    @Autowired
+    public UserController(userService userService, productService productService) {
+        this.userService = userService;
+        this.productService = productService;
+    }
 
-	// 🚀 ROOT URL → Auto Redirect to Login
-	@GetMapping("/")
-	public String redirectHomeToLogin() {
-		return "redirect:/login";
-	}
+    // 🔥 ROOT URL → Auto Redirect to login
+    @GetMapping("/")
+    public String redirectHomeToLogin() {
+        return "redirect:/login";
+    }
 
-	@GetMapping("/login")
-	public ModelAndView userlogin(@RequestParam(required = false) String error) {
-	    ModelAndView mv = new ModelAndView("userLogin");
-	    if ("true".equals(error)) {
-	        mv.addObject("msg", "Please enter correct email and password");
-	    }
-	    return mv;
-	}
+    // 🔥 User Login Page
+    @GetMapping("/login")
+    public ModelAndView userLogin(@RequestParam(required = false) String error) {
+        ModelAndView mv = new ModelAndView("userLogin");
+        if ("true".equals(error)) {
+            mv.addObject("msg", "Please enter correct email and password");
+        }
+        return mv;
+    }
 
-	@GetMapping("/register")
-	public String registerUser() {
-		return "register";
-	}
+    // 🔥 User Registration
+    @GetMapping("/register")
+    public String registerUser() {
+        return "register";
+    }
 
-	@GetMapping("/buy")
-	public String buy() {
-		return "buy";
-	}
+    @PostMapping("/newuserregister")
+    public ModelAndView newUserRegister(@ModelAttribute User user) {
+        boolean exists = this.userService.checkUserExists(user.getUsername());
 
-	@GetMapping("/user/products")
-	public ModelAndView getproduct() {
-		ModelAndView mView = new ModelAndView("uproduct");
-		List<Product> products = this.productService.getProducts();
+        if (!exists) {
+            user.setRole("ROLE_NORMAL");
+            this.userService.addUser(user);
+            return new ModelAndView("userLogin");
+        } else {
+            ModelAndView mv = new ModelAndView("register");
+            mv.addObject("msg", user.getUsername() + " is taken. Please choose a different username.");
+            return mv;
+        }
+    }
 
-		if (products.isEmpty()) {
-			mView.addObject("msg", "No products are available");
-		} else {
-			mView.addObject("products", products);
-		}
-		return mView;
-	}
+    // 🔥 User Dashboard (after login)
+    @GetMapping("/home")
+    public ModelAndView userHome() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("username", username);
 
-	@RequestMapping(value = "newuserregister", method = RequestMethod.POST)
-	public ModelAndView newUseRegister(@ModelAttribute User user) {
-		boolean exists = this.userService.checkUserExists(user.getUsername());
+        List<Product> products = this.productService.getProducts();
+        if (products.isEmpty()) {
+            mv.addObject("msg", "No products are available");
+        } else {
+            mv.addObject("products", products);
+        }
+        return mv;
+    }
 
-		if (!exists) {
-			user.setRole("ROLE_NORMAL");
-			this.userService.addUser(user);
+    // 🔥 Product Page
+    @GetMapping("/user/products")
+    public ModelAndView getProducts() {
+        ModelAndView mv = new ModelAndView("uproduct");
+        List<Product> products = this.productService.getProducts();
 
-			ModelAndView mView = new ModelAndView("userLogin");
-			return mView;
-		} else {
-			ModelAndView mView = new ModelAndView("register");
-			mView.addObject("msg", user.getUsername() + " is taken. Please choose a different username.");
-			return mView;
-		}
-	}
+        if (products.isEmpty()) {
+            mv.addObject("msg", "No products are available");
+        } else {
+            mv.addObject("products", products);
+        }
+        return mv;
+    }
 
-	@GetMapping("/profileDisplay")
-	public String profileDisplay(Model model) {
-		String username = SecurityContextHolder.getContext().getAuthentication().getName();
-		User user = userService.getUserByUsername(username);
+    // 🔥 Buy Page
+    @GetMapping("/buy")
+    public String buy() {
+        return "buy";
+    }
 
-		if (user != null) {
-			model.addAttribute("userid", user.getId());
-			model.addAttribute("username", user.getUsername());
-			model.addAttribute("email", user.getEmail());
-			model.addAttribute("password", user.getPassword());
-			model.addAttribute("address", user.getAddress());
-		} else {
-			model.addAttribute("msg", "User not found");
-		}
-		return "updateProfile";
-	}
+    // 🔥 Profile View
+    @GetMapping("/profileDisplay")
+    public String profileDisplay(Model model) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userService.getUserByUsername(username);
+
+        if (user != null) {
+            model.addAttribute("userid", user.getId());
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            model.addAttribute("password", user.getPassword());
+            model.addAttribute("address", user.getAddress());
+        } else {
+            model.addAttribute("msg", "User not found");
+        }
+        return "updateProfile";
+    }
 }
