781a2cf0be7e820bbff65fb0aa515cb7a93e0c04
hrishikeshkhandagale <hrishikeshkhandagale2@gmail.com>
2025-11-30 10:09:42 +0530

    modified

--- src/main/java/com/jtspringproject/JtSpringProject/controller/ErrorController.java
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/ErrorController.java
@@ -1,20 +1,13 @@
 package com.jtspringproject.JtSpringProject.controller;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class CustomErrorController implements ErrorController {
-
-    @RequestMapping("/error")
-    public String handleError() {
-        return "error";   // error.jsp
-    }
+public class ErrorController {
 
     @GetMapping("/403")
     public String accessDenied() {
-        return "403";     // 403.jsp
+        return "403";
     }
 }
